a6e9530b54921a214c216d245250164171b9ae1c
Kymshay <kymshay@wuevahosting.com>
2022-06-13 10:35:17 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
---------------------
---- BadgerVoting ---
---------------------
+      --------------------
+--- AOP vote edited by Kymshay ---
+      --------------------
 
 voteOptions = {}
 voteNames = {}
@@ -23,8 +23,8 @@ RegisterCommand('startVote', function(source, args, rawCommand)
 					-- It's a number of seconds for voting 
 					local secs = tonumber(args[1]);
 					voteInProgress = true;
-					TriggerClientEvent('chatMessage', -1, prefix .. '^3An AOP vote has been started... You have ^1' 
-						.. args[1] .. ' ^3seconds to vote for the new AOP')
+					TriggerClientEvent('chatMessage', -1, prefix .. '^3Una votacion para cambiar de AOP empezo... Tienes ^1' 
+						.. args[1] .. ' ^3Para votar por el nuevo AOP')
 					for i=2, #args do 
 						TriggerClientEvent('chatMessage', -1, '^6[^7' .. args[i] .. '^6]^3: /vote ' .. (i - 1))
 						numChoices = numChoices + 1;
@@ -41,15 +41,15 @@ RegisterCommand('startVote', function(source, args, rawCommand)
 						end
 					end
 					TriggerClientEvent('chatMessage', -1, prefix .. 
-						'^3The AOP has been decided to be ^1' .. highestVoteName .. ' ^3with a total of ^1' .. highestVoteCount .. ' ^3votes')
+						'^3El AOP cambio a ^1' .. highestVoteName .. ' ^3con un total de ^1' .. highestVoteCount .. ' ^3votos')
 					voteInProgress = false;
 				else 
 					-- Not a valid number supplied
-					sendMsg(source, '^1ERROR: That is not a valid number...')
+					sendMsg(source, '^1ERROR: Eso no es un numero valido...')
 				end
 			else 
 				-- Not enough arguments, need at least 2 choices
-				sendMsg(source, '^1ERROR: You need at least 2 choices... /vote <seconds> Los-Santos Sandy-Shores')
+				sendMsg(source, '^1ERROR: Necesitas al menos dos opciones... /vote <seconds> Los-Santos Sandy-Shores')
 			end
 		end
 	end
@@ -75,22 +75,22 @@ RegisterCommand('vote', function(source, args, rawCommand)
 				if tonumber(args[1]) ~= nil then 
 					-- It's a number 
 					voteOptions[tonumber(args[1])] = voteOptions[tonumber(args[1])] + 1;
-					sendMsg(source, '^3Your vote has been recorded :)')
+					sendMsg(source, '^3Tu voto fue guardado :)')
 					table.insert(votedAlready, source);
 				else 
 					-- It's not a number 
-					sendMsg(source, '^1ERROR: That is not a valid number...')
+					sendMsg(source, '^1ERROR: Eses no es un numero valido...')
 				end
 			else
 				-- They did not supply enough arguments 
-				sendMsg(source, '^1ERROR: You need to do /vote <id>')
+				sendMsg(source, '^1ERROR: Necesitas hacer esto /vote <id>')
 			end
 		else 
 			-- Already voted 
-			sendMsg(source, '^1ERROR: You already voted you cheeky one ;)')
+			sendMsg(source, '^1ERROR: Tu ya habias votado antes ;)')
 		end
 	else 
 		-- There is no vote in progress 
-		sendMsg(source, '^1ERROR: There is currently no vote in progress :(')
+		sendMsg(source, '^1ERROR: No hay ningun voto en progreso :(')
 	end
 end)
