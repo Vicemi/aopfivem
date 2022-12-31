0ea5566b83106aa0b9804d33259e00d4b54a3e17
Kymshay <kymshay@hotmail.com>
2022-12-31 10:55:24 -0300

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,4 +2,4 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_script 'client.lua'
 server_script "server.lua"
--- Kymshay ES | WuevaHosting#0528/wuevahosting.com --
+-- Kymshay | Soluruse.com#0001/soluruse.com --
