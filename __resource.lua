2a15b5d6eaf90589afea2883b3a135b5fca882c0
Vicemi <45047420+Vicemi@users.noreply.github.com>
2023-01-18 10:29:32 -0300

    Chi
    Nos

--- __resource.lua
+++ __resource.lua
@@ -3,3 +3,4 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 client_script 'client.lua'
 server_script "server.lua"
 -- Kymshay | Soluruse.com#0001/soluruse.com --
+-- Vicemi estuvo aqui--
